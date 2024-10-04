56841a5626d3b3bb74c4cc3f354d9ce46587ddd3
besher al addam <beshoux@outlook.fr>
2024-10-04 20:35:52 +0200

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import dev.langchain4j.agent.tool.Tool;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.memory.ChatMemory;
@@ -10,7 +11,9 @@ import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.input.PromptTemplate;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -27,15 +30,15 @@ public class Main {
         /*PromptTemplate promptTemplate = PromptTemplate
                 .from("Tell me a {{adjective}} joke about {{content}}.");
         Map<String, Object> variables = new HashMap<>();
-        variables.put("adjective", "funny");
+        variables.put("adjective", "scary");
         variables.put("content", "computers");
 
         Prompt prompt = promptTemplate.apply(variables);
         String response = model.generate(prompt.text());
-        System.out.println(response); */
+        System.out.println(response);*/
 
         ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(100);
-        chatMemory.add(UserMessage.userMessage("Hello, my name is Luka."));
+        chatMemory.add(UserMessage.userMessage("Hello, my name is Besher."));
         AiMessage answer = model.generate(chatMemory.messages()).content();
         System.out.println(answer.text());
 
